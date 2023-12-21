26f1b1a5af40d6916ed16e65812dd953a379bc8e
MatiaspMorales2001 <143236885+MatiaspMorales2001@users.noreply.github.com>
2023-12-20 23:53:15 -0300

    Delete santivaldi directory

--- santivaldi/static/js/desplegar-nav.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-	var menuIcon = document.getElementById('menuIcon');
-	var menu = document.querySelector('.menu');
-		
-	menuIcon.addEventListener('click', function () {
-	menu.classList.toggle('active');
-				});
-	});
-		
