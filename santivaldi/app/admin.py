cb5d6c1fce70101906c81bddb9e39ff4c6f88e61
MatiaspMorales2001 <143236885+MatiaspMorales2001@users.noreply.github.com>
2023-12-20 23:35:46 -0300

    Delete santivaldi directory

--- santivaldi/app/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import Producto,Trabajador, Formulario
-# Register your models here.
-admin.site.register(Producto)
-admin.site.register(Trabajador)
-admin.site.register(Formulario)
-
